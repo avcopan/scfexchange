5c1248c007f245778c692ba8af8a266b2793037a
avcopan <avcopan@gmail.com>
2017-06-07 12:24:46 -0400

    Tests RPA B-matrix.

--- examples/rpa.py
+++ examples/rpa.py
@@ -29,9 +29,27 @@ class RPA(object):
         a_matrix = a_array.reshape((nocc * nvir, nocc * nvir))
         return a_matrix
 
+    def compute_b_matrix(self):
+        goovv = self.orbitals.get_mo_2e_repulsion(mo_block='o,o,v,v',
+                                                  spin_sector='s,s',
+                                                  antisymmetrize=True)
+        nocc = self.orbitals.get_mo_count(mo_space='o')
+        nvir = self.orbitals.get_mo_count(mo_space='v')
+        b_array = tu.einsum('ijab->iajb', goovv)
+        b_matrix = b_array.reshape((nocc * nvir, nocc * nvir))
+        return b_matrix
+
     def compute_cis_spectrum(self):
         a = self.compute_a_matrix()
         spectrum = spla.eigvalsh(a)
+        return spectrum
+
+    def compute_rpa_spectrum(self):
+        a = self.compute_a_matrix()
+        b = self.compute_b_matrix()
+        h = (a + b).dot(a - b)
+        spectrum = np.sqrt(spla.eigvals(h).real)
+        spectrum.sort()
         return spectrum
 
 
@@ -40,11 +58,6 @@ if __name__ == "__main__":
     from scfexchange.pyscf_interface import Integrals, Orbitals
 
     labels = ("O", "H", "H")
-    '''
-    coordinates = np.array([[0.0000000000, 0.0000000000, -0.1247219248],
-                            [0.0000000000, -1.4343021349, 0.9864370414],
-                            [0.0000000000, 1.4343021349, 0.9864370414]])
-    '''
     coordinates = np.array([[ 0.000000000000, -0.143225816552,  0.000000000000],
                             [ 1.638036840407,  1.136548822547, -0.000000000000],
                             [-1.638036840407,  1.136548822547, -0.000000000000]])
@@ -54,6 +67,6 @@ if __name__ == "__main__":
     orbitals = Orbitals(integrals)
     orbitals.solve()
     rpa = RPA(orbitals)
-    spectrum = rpa.compute_cis_spectrum()
+    spectrum = rpa.compute_rpa_spectrum()
     print(spectrum)
 
--- test/test_example_rpa.py
+++ test/test_example_rpa.py
@@ -26,3 +26,33 @@ def test__rpa_cis_spectrum():
         20.0505319444
     ]
     assert (np.allclose(spectrum, ref_spectrum))
+
+
+def test__rpa_rpa_spectrum():
+    import numpy as np
+    from scfexchange import Nuclei
+    from scfexchange.pyscf_interface import Integrals, Orbitals
+    from scfexchange.examples.rpa import RPA
+
+    labels = ("O", "H", "H")
+    coordinates = np.array([[0.00000000000, -0.14322581655, 0.00000000000],
+                            [1.63803684041, 1.13654882255, 0.00000000000],
+                            [-1.63803684041, 1.13654882255, 0.00000000000]])
+    nuclei = Nuclei(labels, coordinates)
+    integrals = Integrals(nuclei, "sto-3g")
+    orbitals = Orbitals(integrals)
+    orbitals.solve()
+    rpa = RPA(orbitals)
+    spectrum = rpa.compute_rpa_spectrum()
+    ref_spectrum = [
+        0.2851637170, 0.2851637170, 0.2851637170, 0.2997434467, 0.2997434467,
+        0.2997434467, 0.3526266606, 0.3526266606, 0.3526266606, 0.3547782530,
+        0.3651313107, 0.3651313107, 0.3651313107, 0.4153174946, 0.5001011401,
+        0.5106610509, 0.5106610509, 0.5106610509, 0.5460719086, 0.5460719086,
+        0.5460719086, 0.5513718846, 0.6502707118, 0.8734253708, 1.1038187957,
+        1.1038187957, 1.1038187957, 1.1957870714, 1.1957870714, 1.1957870714,
+        1.2832053178, 1.3237421886, 19.9585040647, 19.9585040647, 19.9585040647,
+        20.0109471551, 20.0113074586, 20.0113074586, 20.0113074586,
+        20.0504919449
+    ]
+    assert (np.allclose(spectrum, ref_spectrum))
